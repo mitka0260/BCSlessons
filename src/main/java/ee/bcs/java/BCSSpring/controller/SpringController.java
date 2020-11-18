e1fc82dcee1c630dab2b19602bbd47671b3553b6
mitka0260 <mitkaut@gmail.com>
2020-11-18 20:52:52 +0200

    edited imports

--- src/main/java/ee/bcs/java/BCSSpring/controller/SpringController.java
+++ src/main/java/ee/bcs/java/BCSSpring/controller/SpringController.java
@@ -2,6 +2,8 @@ package ee.bcs.java.BCSSpring.controller;
 
 import ee.bcs.java.lessons.Lesson1;
 import ee.bcs.java.lessons.Lesson3;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -9,10 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class SpringController {
+
+    //сам подключится к базе данных
+    @Autowired
+    private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
     //аннотация, работает сервер спринга, запускает мой код, ищет весь мой код
     //контроллер - это важный для нас класс, и это спринг знает
@@ -177,9 +185,11 @@ public class SpringController {
 
 
     List<Employee> employeeList = new ArrayList<>();
+    //шлобальная переменная, к ней могут обращаться все методы
 
     @GetMapping("Employees") //returns all employees
-    public List showAllEmployees() {
+    public List showAllEmployees()
+    {
         return employeeList;
     }
     //http://localhost:8080/Employees
@@ -233,7 +243,29 @@ public class SpringController {
             @PathVariable("index") Integer index) {
         return employeeList.get(index);
     }
-    //http://localhost:8080/Employee/3
+    //http://localhost:8080/Employee/index/3
+
+    @GetMapping("Employee/indexDB/{index}") //show employee by index in the List
+    public String showEmployeeByIndex1(
+            @PathVariable("index") Integer index) {
+        String sql = "SELECT name FROM employee WHERE id = " + index;
+        String result = namedParameterJdbcTemplate.queryForObject(sql, new HashMap<>(), String.class);
+        return result;
+    }
+    //http://localhost:8080/Employee-indexDB/3
+
+    @GetMapping("employee/getName")
+    public String getEmployee(@RequestParam("id") int idFromRequest) {
+        String sql = "SELECT name FROM employee WHERE id = :idSql";
+        Map<String, Object> paramMap = new HashMap<>();
+        paramMap.put("idSql", idFromRequest);
+        String result = namedParameterJdbcTemplate.queryForObject(sql, paramMap, String.class);
+        return result;
+
+        //методы селект и др ничего не возвращают, тога надо так:
+        //namedParameterJdbcTemplate.update(sql, paramMap);
+    }
+//WHERE id =" + idFromRequest; - так писать нельзя в целях безопасности!!!
 
     @GetMapping("Employee/id/{id}") //show employee by id
     public String showEmployeeById(
@@ -300,5 +332,12 @@ public class SpringController {
         employeeList.remove(id);
     }
     //http://localhost:8080/Employee/delete?id=1
+
+    /*
+    String sql = "SELECT amount FROM account WHERE id = :id";
+    Map<String, Object> paramMap = new HashMap<>();
+    paramMap.put("id", id);
+    Integer dbAmount = namedParameterJdbcTemplate.queryForObject(sql, paramMap, Integer.class);
+    */
 
 }
