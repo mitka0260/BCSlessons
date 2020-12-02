b4fb5e4dbe239be4db1cd4fbf5d7d0feb02653ef
mitka0260 <mitkaut@gmail.com>
2020-12-02 16:53:45 +0200

    unit test examples added

--- src/main/java/proselyte/ArrayHolderDemo.java
+++ src/main/java/proselyte/ArrayHolderDemo.java
@@ -1,9 +1,12 @@
 package proselyte;
 
+import java.util.Arrays;
+
 public class ArrayHolderDemo {
 
     public static void main(String[] args) {
         ArrayHolder arrayHolder = new ArrayHolder();
         System.out.println(arrayHolder);
     }
+    
 }
